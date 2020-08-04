4e64bf185b0717f75e5722304dcd159970edc4fe
Elismar13 <elismarsilva.5@outlook.com>
2020-08-04 12:13:34 -0300

    refactor: 💡 Landing page main styles

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,11 +1,51 @@
 import React from 'react';
 
+import logoImg from '../../assets/images/logo.svg';
+import landingImg from '../../assets/images/landing.svg';
+import studyIcon from '../../assets/images/icons/study.svg';
+import giveClassification from '../../assets/images/icons/give-classes.svg';
+import purpleHeart from '../../assets/images/icons/purple-heart.svg';
+
+import { 
+    PageContainer,
+    PageLandingContent,
+    LogoContainer,
+    HeroImage,
+    ButtonsContainer,
+    Study,
+    GiveClases,
+    TotalConnections 
+    } from './styled';
+
+
 const Landing: React.FC = () => {
     return (
-        <div>
-            <h1>Hello, world!</h1>
-        </div>
-    )
+        <PageContainer>
+            <PageLandingContent>
+                <LogoContainer>
+                    <img src={logoImg} alt="Proffy"/>
+                    <h2>Sua plataforma de estudos online</h2>
+                </LogoContainer>
+
+                <HeroImage src={landingImg} alt="Plataforma de estudos"/>
+
+                <ButtonsContainer>
+                    <Study>
+                        <img src={studyIcon} alt="Estudar"/>
+                        Estudar
+                    </Study>
+                    <GiveClases>
+                        <img src={giveClassification} alt="Estudar"/>
+                        Estudar
+                    </GiveClases>
+                </ButtonsContainer>
+
+                <TotalConnections>
+                    total de 200 conexões realizadas <img src={purpleHeart} alt="Coração Roxo"/> 
+                </TotalConnections>
+            </PageLandingContent>
+        </PageContainer>
+    );
 }
 
 export default Landing;
--- src/pages/Landing/styled.ts
+++ src/pages/Landing/styled.ts
@@ -1,5 +1,33 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
-  
+export const PageContainer = styled.div`
+
+`;
+
+export const PageLandingContent = styled.div`
+
+`;
+
+export const LogoContainer = styled.div`
+
+`;
+
+export const HeroImage = styled.img`
+
+`;
+
+export const ButtonsContainer = styled.div`
+
+`;
+
+export const Study = styled.a`
+
+`;
+
+export const GiveClases = styled.a`
+
+`;
+
+export const TotalConnections = styled.span`
+
 `;
